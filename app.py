22bc328efe9cb76e680497e1a890944675621741
Harshitha Narisetty <narisettyharshitha00@gmail.com>
2025-07-06 01:18:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -440,5 +440,5 @@ def book_appointment():
 # ---------------------------------------
 # MAIN
 # ---------------------------------------
-if __name__ == "__main__":
-    app.run(debug=True)
+if _name_ == "_main_":
+    app.run(host='0.0.0.0', port=5000, debug=True)
